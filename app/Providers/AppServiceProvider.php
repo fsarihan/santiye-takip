4c41a0f8a3159cd92d225dbd4c49375730d4a0af
fsarihan <furkansarihan@gmail.com>
2019-12-28 14:59:34 +0300

    first model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,27 @@
 <?php
-
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-}
+	
+	namespace App\Providers;
+	
+	use Illuminate\Support\ServiceProvider;
+	use Illuminate\Database\Schema\Builder;
+	
+	class AppServiceProvider extends ServiceProvider
+	{
+		/**
+		 * Register any application services.
+		 *
+		 * @return void
+		 */
+		public function register() {
+			//
+		}
+		
+		/**
+		 * Bootstrap any application services.
+		 *
+		 * @return void
+		 */
+		public function boot() {
+			Builder::defaultStringLength(191);
+		}
+	}
--- app/User.php
+++ app/User.php
@@ -1,39 +1,26 @@
 <?php
-
-namespace App;
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-}
+	
+	namespace App\Model;
+	
+	use Illuminate\Notifications\Notifiable;
+	
+	class User extends \Cartalyst\Sentinel\Users\EloquentUser
+	{
+		use Notifiable;
+		
+		protected $fillable = [
+			'email',
+			'password',
+			'permissions',
+			'adi',
+			'last_login',
+			'sirket_id',
+			'telefon',
+			'soyadi',
+		
+		];
+		
+		//public function getOrganization() {
+		//	return $this->hasOne('App\Model\Organization', 'id', 'organization_id');
+		//}
+	}
